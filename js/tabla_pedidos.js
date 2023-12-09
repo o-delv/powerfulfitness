d2a50ad0a87b7b92e38cafe1b783df3e69f2d2ce
octavio <112964308+o-delv@users.noreply.github.com>
2023-12-09 15:16:53 -0300

    Update tabla_pedidos.js

--- js/tabla_pedidos.js
+++ js/tabla_pedidos.js
@@ -3,7 +3,7 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        url:"http://127.0.0.1:5000/pedidos",
+        url:"https://powerfulfitness.pythonanywhere.com/pedidos",
         pedidos:[],
         error:false,
         cargando:true
@@ -31,7 +31,7 @@ const { createApp } = Vue
         // el id se necesita para buscar en la DB y eliminarlo
         eliminar(id) {
             
-            const url = 'http://localhost:5000/borrar/'+id;
+            const url = 'https://powerfulfitness.pythonanywhere.com/borrar/'+id;
             var options = {
                 method: 'DELETE',
                 
